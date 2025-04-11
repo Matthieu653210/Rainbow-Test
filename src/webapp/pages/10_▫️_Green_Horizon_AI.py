6165e721ccd6dbfc43f7cd927638bc61d782e4d2
thierry-caminel-atos <thierry.caminel@atos.net>
2025-04-11 22:09:20 +0200

    refactor: reorganize and clean up green horizon ai chat page

--- src/webapp/pages/10_▫️_Green_Horizon_AI.py
+++ src/webapp/pages/10_▫️_Green_Horizon_AI.py
@@ -16,11 +16,7 @@ from src.utils.config_mngr import global_config
 from src.webapp.ui_components.llm_config import llm_config_widget
 from src.webapp.ui_components.smoloagents_streamlit import stream_to_streamlit
 
-# MODEL_ID = "gpt_4o_azure"
-MODEL_ID = None
-
-
-# SmolagentsInstrumentor().instrument(tracer_provider=get_telemetry_trace_provider())
+MODEL_ID = None  # Use the one by configuration
 
 SAMPLE_PROMPTS = [
     "what was the CO2 emissions of Brazil for energy generation in 2023",
@@ -30,21 +26,10 @@ SAMPLE_PROMPTS = [
 ]
 
 
-st.title("Green Horizon AI Chat")
-# st.logo(str(Path.cwd() / "src/webapp/static/eviden-logo-white.png"), size="large")
-llm_config_widget(st.sidebar)
-
-
-model_name = LlmFactory(llm_id=MODEL_ID).get_litellm_model_name()
-llm = LiteLLMModel(model_id=model_name)
-print(model_name)
-
-
 @st.cache_resource(show_spinner="Load data files")
 def get_data() -> pd.DataFrame:
     data_file = global_config().get_path("datasets_root") / "carbon-monitor-data 1.xlsx"
     assert data_file.exists()
-
     if data_file.name.endswith(".csv"):
         df = pd.read_csv(data_file, decimal=",")
     else:
@@ -58,22 +43,29 @@ def get_data_frame() -> pd.DataFrame:
     return get_data()
 
 
-st.title("📊 Carbon Agent")
+st.title("Green Horizon AI Chat")
+llm_config_widget(st.sidebar)
+
+model_name = LlmFactory(llm_id=MODEL_ID).get_litellm_model_name()
+llm = LiteLLMModel(model_id=model_name)
 
 with st.expander(label="Prompt examples", expanded=True):
     st.write(SAMPLE_PROMPTS)
 
 PRE_PROMPT = dedent_ws("""
-    Answser following question.
-    Write your final anwser using streamlit, ie by ending by 'st.write(...)\n'
+    Answer following question.
+    Write your final answer using streamlit, ie using 'st.write(...)' or equivalent.\n
+    End by printing the result on stdio, or the title if it's a diagram.
     """)
 
-if prompt := st.chat_input("What would you like to ask SmolAgents?"):
-    agent = CodeAgent(
-        tools=[get_data_frame, DuckDuckGoSearchTool(), VisitWebpageTool()],
-        model=llm,
-        additional_authorized_imports=["pandas", "matplotlib.pyplot", "numpy", "json", "streamlit"],
-    )
+col1, col2 = st.columns(2)
+with col1:
+    if prompt := st.chat_input("What would you like to ask SmolAgents?"):
+        agent = CodeAgent(
+            tools=[get_data_frame, DuckDuckGoSearchTool(), VisitWebpageTool()],
+            model=llm,
+            additional_authorized_imports=["pandas", "matplotlib.pyplot", "numpy", "json", "streamlit"],
+        )
 
-    with st.container(height=600):
-        stream_to_streamlit(agent, PRE_PROMPT + prompt, additional_args={"st": st})
+        with st.container(height=600):
+            stream_to_streamlit(agent, PRE_PROMPT + prompt, additional_args={"st": col2})
