69132d80efec61ba07c49fcdf7e4db5fd3c44ac6
thierry-caminel-atos <thierry.caminel@atos.net>
2025-04-14 21:20:00 +0200

    feat: add Streamlit application file

---
+++ src/main/streamlit.py
@@ -0,0 +1,35 @@
+# Add comments AI!
+import streamlit as st
+from dotenv import load_dotenv
+from loguru import logger
+
+from src.utils.config_mngr import config_loguru, global_config
+
+load_dotenv(verbose=True)
+config_loguru()
+logger.info("Start Webapp...")
+
+st.set_page_config(
+    page_title=global_config().get_str("ui.app_name"),
+    page_icon="🛠️",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+
+pages_dir = global_config().get_path("ui.pages_dir")
+# Sort files by the number at the beginning of their name
+pages_fn = sorted(
+    pages_dir.glob("*.py"), key=lambda f: int(f.name.split("_")[0]) if f.name.split("_")[0].isdigit() else 0
+)
+pages = [st.Page(f.absolute()) for f in pages_fn if f.name != "__init__.py"]
+pg = st.navigation(pages)
+pg.run()
+
+
+# def main() -> None:
+#     # taken from https://blog.yericchen.com/python/installable-streamlit-app.html
+#     # Does not work as expected
+#     script_path = __file__
+#     import subprocess
+#     subprocess.run(["streamlit", "run", script_path])
